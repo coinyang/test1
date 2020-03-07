5d7c4db486dc63e933fdf011def04b7194b86212
740133670@qq.com <Administrator@windows10.microdone.cn>
2020-03-07 16:45:30 +0800

    1912demo练习

---
+++ vip1912/src/day01/Demo1.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("helloworld");
+		//整數類型
+		int age = 23;
+		int w23$_;
+		// 小數類型
+		double d = 1.3;
+		/*
+		 * gngfhmjh
+		 * vjmgjhkl'
+		 * ghk
+		 */
+
+	}
+
+}
